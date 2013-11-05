9c6ceeceabf8abbe1c41cc07aed45488c1f0b939
Michael Berkovich <theiceberk@gmail.com>
2013-11-05 15:20:54 -0800

    latest tr8n

--- tr8n/tr8n.js
+++ tr8n/tr8n.js
@@ -4,7 +4,7 @@
         var script = doc.createElement('script');
         script.setAttribute('id', 'tr8n_root');
         script.setAttribute('type', 'application/javascript');
-        script.setAttribute('src', '//' + response.host + '/tr8n/api/v1/proxy/boot.js?text=true&immediate=true');
+        script.setAttribute('src', '//' + response.host + '/tr8n/api/proxy/boot.js?text=true&immediate=true');
         script.setAttribute('charset', 'UTF-8');
         doc.getElementsByTagName('head')[0].appendChild(script);
       }
