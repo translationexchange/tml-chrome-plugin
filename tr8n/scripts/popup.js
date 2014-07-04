cda2e0a2d7dcc8b0ba61c9ef1f74f6952ece19ce
Ian McDaniel <ianmcdaniel@gmail.com>
2014-07-04 12:08:37 -0700

    added login signup panels

--- tr8n/scripts/popup.js
+++ tr8n/scripts/popup.js
@@ -1,14 +1,16 @@
 var PopupController = function () {
-  this.toggle_button_ = document.getElementById('toggle_button');
-  this.note_ = document.getElementById('note');
-
-  this.host_ = document.getElementById('host');
-  this.host_form_ = document.getElementById('host_form');
-  this.host_field_ = document.getElementById('host_field');
-  this.host_button_ = document.getElementById('host_button');
+  this.toggle_button_ = $('#toggle_button');
+  this.note_          = $('#note');
+  this.host_          = $('#host');
+  this.host_form_     = $('#host_form');
+  this.host_field_    = $('#host_field');
+  this.host_button_   = $('#host_button');
+  this.auth_forms     = $('.auth-form form');
 
   this.updateLabels();
   this.addListeners_();
+
+  this.showView('login');
 };
 
 PopupController.prototype = {
@@ -32,20 +34,23 @@ PopupController.prototype = {
    * @private
    */
   addListeners_: function() {
-    this.toggle_button_.addEventListener('click', this.toggleTr8n_.bind(this));
-    this.host_.addEventListener('dblclick', this.showHostForm_.bind(this));
-    this.host_button_.addEventListener('click', this.setHost_.bind(this));
+    this.auth_forms.on(     'submit',   this.submitForm.bind(this)).validate();
+    this.toggle_button_.on( 'click',    this.toggleTr8n_.bind(this));
+    this.host_.on(          'dblclick', this.showHostForm_.bind(this));
+    this.host_button_.on(   'click',    this.setHost_.bind(this));
+    
+    $('[data-show-view]').on('click',   this.showView.bind(this));
   },
 
   updateLabels: function() {
     var bgPage = chrome.extension.getBackgroundPage();
-    this.host_.innerHTML = bgPage.tr8n_host;
+    this.host_.html(bgPage.tr8n_host);
     if (bgPage.tr8n_enabled) {
-      this.note_.style.display = "block";
-      this.toggle_button_.innerHTML = "Deactivate";
+      this.note_.show()
+      this.toggle_button_.text("Deactivate");
     } else {
-      this.note_.style.display = "none";
-      this.toggle_button_.innerHTML = "Activate";
+      this.note_.hide()
+      this.toggle_button_.text("Activate");
     }
   },
 
@@ -57,17 +62,17 @@ PopupController.prototype = {
 
   showHostForm_: function() {
     var bgPage = chrome.extension.getBackgroundPage();
-    this.host_field_.value = bgPage.tr8n_host;
-    this.host_.style.display = "none";
-    this.host_form_.style.display = "block";
+    this.host_field_.val(bgPage.tr8n_host);
+    this.host_.hide()
+    this.host_form_.show()
   },
 
   setHost_: function () {
     var bgPage = chrome.extension.getBackgroundPage();
-    bgPage.tr8n_host = this.host_field_.value;
+    bgPage.tr8n_host = this.host_field_.val();
 
-    this.host_.style.display = "block";
-    this.host_form_.style.display = "none";
+    this.host_.show();
+    this.host_form_.hide();
     this.updateLabels();
     this.reloadWindow();
   },
@@ -77,10 +82,70 @@ PopupController.prototype = {
       var code = 'window.location.reload();';
       chrome.tabs.executeScript(tab.id, {code: code});
     });
+  },
+
+  submitForm: function(e) {
+    var frm = $(e.currentTarget);
+    e.preventDefault();
+    if(!frm.valid()) return;
+    $.ajax({
+      type    : "post",
+      url     : frm.attr('action'),
+      data    : frm.serialize(),
+      success : this[$.camelCase("handle-"+frm.attr('class'))],
+      error   : this[$.camelCase("handle-"+frm.attr('class')+"-error")]
+    });
+  },
+
+
+  handleLoginForm: function(data) {
+    showView('main');
+  },
+
+  handleLoginFormError: function(xhr) {
+    var error = $('<div>').addClass('flash-error').text("Something went wrong!")
+    $('#login').find('.flash-error').remove().end().prepend(error);
+  },
+
+  handleSignupForm: function(data) {
+    showView('confirm')
+  },
+
+  handleSignupFormError: function(xhr) {
+  },
+
+  handleConfirmForm: function(data) {
+    showView('registration')
+  },
+  
+  handleConfirmFormError: function(xhr) {
+  },
+
+  handleRegistrationForm: function(data) {
+    showView('main')
+  },
+
+  handleRegistrationFormError: function(data) {
+  },
+
+  showView: function(name){
+    if (name.currentTarget) {
+      name = $(name.currentTarget).data('show-view')
+    }
+    $('.flash-error').remove();
+    $('body').attr('class',name);
   }
 
 };
 
-document.addEventListener('DOMContentLoaded', function () {
+$(function () {
   window.PC = new PopupController();
 });
+
+
+
+
+
+
+
+
