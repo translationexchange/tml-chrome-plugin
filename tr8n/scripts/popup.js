1bc892ad2dde6f77585f1273fd8570038b04878c
Michael Berkovich <theiceberk@gmail.com>
2014-07-05 17:27:09 -0700

    added sso

--- tr8n/scripts/popup.js
+++ tr8n/scripts/popup.js
@@ -7,10 +7,18 @@ var PopupController = function () {
   this.host_button_   = $('#host_button');
   this.auth_forms     = $('.auth-form form');
 
+  this.oauth_facebook_button_ = $('#oauth_facebook');
+  this.oauth_twitter_button_ = $('#oauth_twitter');
+  this.oauth_google_button_ = $('#oauth_google_oauth2');
+  this.oauth_github_button_ = $('#oauth_github');
+  this.oauth_linkedin_button_ = $('#oauth_linkedin');
+  this.oauth_vkontakte_button_ = $('#oauth_vkontakte');
+
   this.updateLabels();
   this.addListeners_();
 
-  this.showView('login');
+//  this.showView('login');
+  this.showView('main');
 };
 
 PopupController.prototype = {
@@ -27,6 +35,13 @@ PopupController.prototype = {
   host_field_: null,
   host_button_: null,
 
+  oauth_facebook_button_: null,
+  oauth_twitter_button_: null,
+  oauth_google_button_: null,
+  oauth_github_button_: null,
+  oauth_linkedin_button_: null,
+  oauth_vkontakte_button_: null,
+
   /**
    * Adds event listeners to the button in order to capture a user's click, and
    * perform some action in response.
@@ -38,12 +53,45 @@ PopupController.prototype = {
     this.toggle_button_.on( 'click',    this.toggleTr8n_.bind(this));
     this.host_.on(          'dblclick', this.showHostForm_.bind(this));
     this.host_button_.on(   'click',    this.setHost_.bind(this));
-    
+
+    this.oauth_facebook_button_.on(   'click',    this.oauth_.bind(this));
+    this.oauth_twitter_button_.on(   'click',    this.oauth_.bind(this));
+    this.oauth_google_button_.on(   'click',    this.oauth_.bind(this));
+    this.oauth_github_button_.on(   'click',    this.oauth_.bind(this));
+    this.oauth_linkedin_button_.on(   'click',    this.oauth_.bind(this));
+    this.oauth_vkontakte_button_.on(   'click',    this.oauth_.bind(this));
+
     $('[data-show-view]').on('click',   this.showView.bind(this));
   },
 
+  getBackgroundPage: function() {
+    return chrome.extension.getBackgroundPage();
+  },
+
+  log: function(msg) {
+    this.getBackgroundPage().console.log('TranslationExchange: ' + msg);
+  },
+
+  oauth_: function(e) {
+    var provider = e.currentTarget.id.replace("oauth_", "");
+    this.log(provider);
+    var w = 800, h = 600;
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2);
+    var host = this.getBackgroundPage().tr8n_host;
+
+    var url = host + "/login/auth?provider=" + provider + "&display=window";
+    if (host.indexOf("localhost") != -1 || host.indexOf("lvh.me") != -1)
+      url = "http://" + url;
+    else
+      url = "https://" + url;
+
+    var win = window.open(url, "oauth", 'toolbar=1, location=1, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+    return false;
+  },
+
   updateLabels: function() {
-    var bgPage = chrome.extension.getBackgroundPage();
+    var bgPage = this.getBackgroundPage();
     this.host_.html(bgPage.tr8n_host);
     if (bgPage.tr8n_enabled) {
       this.note_.show()
@@ -55,9 +103,13 @@ PopupController.prototype = {
   },
 
   toggleTr8n_: function() {
-    chrome.extension.getBackgroundPage().tr8n_enabled = !chrome.extension.getBackgroundPage().tr8n_enabled;
-    this.updateLabels();
-    this.reloadWindow();
+    if (true) {
+      this.showView('login');
+    } else {
+      chrome.extension.getBackgroundPage().tr8n_enabled = !chrome.extension.getBackgroundPage().tr8n_enabled;
+      this.updateLabels();
+      this.reloadWindow();
+    }
   },
 
   showHostForm_: function() {
